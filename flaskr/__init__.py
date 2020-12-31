eed92aefd8d9966bba236632197a74c69aa52d9d
Selim Onat <selim.onat@mytaxi.com>
2020-12-31 01:51:10 +0100

    routes add end-point URLs to the the summary.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,18 @@ from skyjo.skyjo import Skyjo
 
 game = Skyjo()
 
+mapper = dict()
+mapper["start_game"] = 'http://127.0.0.1:5000/start_game'
+mapper["update_table"] = 'http://127.0.0.1:5000/update_table'
+mapper[f"open_card"] = list()
+for n in range(12):
+    mapper["open_card"].append(f"http://127.0.0.1:5000/open_card/{n}")
+mapper['exchange_card'] = list()
+for n in range(12):
+    mapper["exchange_card"].append(f"http://127.0.0.1:5000/exchange_card/{n}")
+
+
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -30,23 +42,52 @@ def create_app(test_config=None):
 
     @app.route('/')
     def landing_page():
-        return str([f.__name__ for f in game.possible_actions])
+
+        summary = game.summary()
+        for action in summary['possible_actions'].keys():
+            summary["possible_actions"][action] = mapper[action]
+
+        return summary
 
     @app.route('/start_game')
     def start_game():
-        # TODO: just show the user info with hyperlinks to actions.
-        return game.start_game()
+
+        game.start_game()
+
+        summary = game.summary()
+        for action in summary['possible_actions'].keys():
+            summary["possible_actions"][action] = mapper[action]
+
+        return summary
 
     @app.route('/exchange_card/<id>')
-    def _hand_table_exchange(id):
-        return game.exchange_card(int(id))
+    def exchange_card(id):
+
+        game.exchange_card(int(id))
+        summary = game.summary()
+        for action in summary['possible_actions'].keys():
+            summary["possible_actions"][action] = mapper[action]
+
+        return summary
 
     @app.route('/update_table')
-    def _deck_to_table():
-        return game.update_table()
+    def update_table():
+
+        game.update_table()
+        summary = game.summary()
+        for action in summary['possible_actions'].keys():
+            summary["possible_actions"][action] = mapper[action]
+
+        return summary
 
     @app.route('/open_card/<id>')
-    def _open_card(id):
-        return game.open_card(int(id))
+    def open_card(id):
+
+        game.open_card(int(id))
+        summary = game.summary()
+        for action in summary['possible_actions'].keys():
+            summary["possible_actions"][action] = mapper[action]
+
+        return summary
 
     return app
